dc97f438276c7aa093e6f9625da488f95749915b
kristofersokk <kristofer.sokk@gmail.com>
2017-10-18 00:30:02 +0300

    fixed shift-click

--- src/main/java/com/timotheteus/raincontrol/gui/container/ContainerGenerator.java
+++ src/main/java/com/timotheteus/raincontrol/gui/container/ContainerGenerator.java
@@ -1,16 +1,16 @@
 package com.timotheteus.raincontrol.gui.container;
 
+import com.timotheteus.raincontrol.gui.CustomSlot;
 import com.timotheteus.raincontrol.tileentities.TileEntityGeneratorBlock;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.SlotItemHandler;
-
-import javax.annotation.Nullable;
 
 public class ContainerGenerator extends Container {
 
@@ -42,6 +42,8 @@ public class ContainerGenerator extends Container {
         }
     }
 
+    private static final CustomSlot.StackFilter filter = CustomSlot.StackFilter.GENERATOR;
+
     private void addOwnSlots() {
         IItemHandler itemHandler = this.te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
         int x = 70;
@@ -50,38 +52,65 @@ public class ContainerGenerator extends Container {
         // Add our own slots
         int slotIndex = 0;
         for (int i = 0; i < itemHandler.getSlots(); i++) {
-            addSlotToContainer(new SlotItemHandler(itemHandler, slotIndex, x, y));
+            addSlotToContainer(new CustomSlot(itemHandler, slotIndex, x, y, filter));
             slotIndex++;
             x += 18;
         }
     }
 
-    @Nullable
+    @Override
+    public ItemStack slotClick(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player) {
+        return super.slotClick(slotId, dragType, clickTypeIn, player);
+    }
+
     @Override
     public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
-        ItemStack itemstack = null;
-        Slot slot = this.inventorySlots.get(index);
+        int inventoryStart = 1;
+        int inventoryEnd = inventoryStart + 26;
+        int hotbarStart = inventoryEnd + 1;
+        int hotbarEnd = hotbarStart + 8;
 
-        if (slot != null && slot.getHasStack()) {
-            ItemStack itemstack1 = slot.getStack();
-            itemstack = itemstack1.copy();
+        Slot theSlot = this.inventorySlots.get(index);
 
-            if (index < TileEntityGeneratorBlock.SIZE) {
-                if (!this.mergeItemStack(itemstack1, TileEntityGeneratorBlock.SIZE, this.inventorySlots.size(), true)) {
-                    return null;
+        if (theSlot != null && theSlot.getHasStack()) {
+            ItemStack newStack = theSlot.getStack();
+            ItemStack currentStack = newStack.copy();
+
+            //Other Slots in Inventory excluded
+            if (index >= inventoryStart) {
+                //Shift from Inventory
+                if (TileEntityFurnace.getItemBurnTime(newStack) > 0) {
+                    if (!this.mergeItemStack(newStack, 0, 1, false)) {
+                        return ItemStack.EMPTY.copy();
+                    }
                 }
-            } else if (!this.mergeItemStack(itemstack1, 0, TileEntityGeneratorBlock.SIZE, false)) {
-                return null;
+                //
+
+                else if (index >= inventoryStart && index <= inventoryEnd) {
+                    if (!this.mergeItemStack(newStack, hotbarStart, hotbarEnd + 1, false)) {
+                        return ItemStack.EMPTY.copy();
+                    }
+                } else if (index >= inventoryEnd + 1 && index < hotbarEnd + 1 && !this.mergeItemStack(newStack, inventoryStart, inventoryEnd + 1, false)) {
+                    return ItemStack.EMPTY.copy();
+                }
+            } else if (!this.mergeItemStack(newStack, inventoryStart, hotbarEnd + 1, false)) {
+                return ItemStack.EMPTY.copy();
             }
 
-            if (itemstack1.isEmpty()) {
-                slot.putStack(ItemStack.EMPTY);
+            if (!CustomSlot.isValid(newStack, filter)) {
+                theSlot.putStack(ItemStack.EMPTY.copy());
             } else {
-                slot.onSlotChanged();
+                theSlot.onSlotChanged();
             }
-        }
 
-        return itemstack;
+            if (newStack.getCount() == currentStack.getCount()) {
+                return ItemStack.EMPTY.copy();
+            }
+            theSlot.onTake(playerIn, newStack);
+
+            return currentStack;
+        }
+        return ItemStack.EMPTY.copy();
     }
 
     @Override
