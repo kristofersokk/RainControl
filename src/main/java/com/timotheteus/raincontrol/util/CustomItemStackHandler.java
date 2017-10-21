1f923cc76701489d1a228ee64a9b1519a27706bb
kristofersokk <kristofer.sokk@gmail.com>
2017-10-21 15:10:59 +0300

    now implements IInventory

--- src/main/java/com/timotheteus/raincontrol/util/CustomItemStackHandler.java
+++ src/main/java/com/timotheteus/raincontrol/util/CustomItemStackHandler.java
@@ -1,24 +1,22 @@
 package com.timotheteus.raincontrol.util;
 
+import com.timotheteus.raincontrol.gui.CustomSlot;
 import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.items.ItemStackHandler;
 
+import javax.annotation.Nonnull;
+
 public class CustomItemStackHandler extends ItemStackHandler {
 
-    public CustomItemStackHandler() {
-    }
+    private CustomSlot.StackFilter filter;
 
-    public CustomItemStackHandler(int size) {
+    protected CustomItemStackHandler(int size, CustomSlot.StackFilter filter) {
         super(size);
-    }
-
-    public CustomItemStackHandler(NonNullList<ItemStack> stacks) {
-        super(stacks);
+        this.filter = filter;
     }
 
     public void dropInventory(World world, BlockPos pos) {
@@ -28,12 +26,10 @@ public class CustomItemStackHandler extends ItemStackHandler {
     }
 
     /**
-     * @param slot
-     * @param modify
      * @return stack changed
      */
     public boolean modifyStack(int slot, int modify) {
-        ItemStack stack = null;
+        ItemStack stack;
         try {
             stack = getStackInSlot(slot).copy();
         } catch (Exception e) {
@@ -45,7 +41,7 @@ public class CustomItemStackHandler extends ItemStackHandler {
             if (newAmount > stack.getMaxStackSize())
                 newAmount = stack.getMaxStackSize();
             if (newAmount < 1) {
-                setStackInSlot(slot, new ItemStack((Item) null));
+                setStackInSlot(slot, ItemStack.EMPTY.copy());
             } else {
                 stack.setCount(newAmount);
                 setStackInSlot(slot, stack);
@@ -53,5 +49,93 @@ public class CustomItemStackHandler extends ItemStackHandler {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void validateSlotIndex(int slot) {
+        super.validateSlotIndex(slot);
+    }
+
+    public NonNullList<ItemStack> getAllSlots() {
+        return stacks;
+    }
+
+    @Override
+    public void setSize(int size) {
+    }
+
+    @Override
+    public void setStackInSlot(int slot, @Nonnull ItemStack stack) {
+        stacks.set(slot, stack.copy());
+    }
+
+    @Override
+    public int getSlots() {
+        return stacks.size();
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack getStackInSlot(int slot) {
+        return stacks.get(slot);
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+        ItemStack local = getStackInSlot(slot);
+        if (filter.isValid(stack) && (local.isEmpty() || ItemStack.areItemsEqual(local, stack))) {
+            int stacklimit = getStackLimit(slot, stack);
+            if (local.getCount() == stacklimit) {
+                return stack;
+            } else if (stack.getCount() + local.getCount() <= stacklimit) {
+                if (!simulate) {
+                    ItemStack newStack = stack.copy();
+                    newStack.setCount(stack.getCount() + local.getCount());
+                    setStackInSlot(slot, newStack);
+                }
+                return ItemStack.EMPTY;
+            } else {//there's a remainder
+                ItemStack newStack = local.copy();
+                int remainder = local.getMaxStackSize() - local.getCount();
+                if (!simulate)
+                    local.setCount(local.getMaxStackSize());
+                newStack.setCount(remainder);
+                return newStack;
+            }
+        }
+        return stack;
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack extractItem(int slot, int amount, boolean simulate) {
+        ItemStack local = getStackInSlot(slot);
+        ItemStack newStack = local.copy();
+        ItemStack newLocal = local.copy();
+        if (local.getCount() == 0 || amount == 0) {
+            return ItemStack.EMPTY;
+        } else if (amount >= local.getCount()) {
+            newStack.setCount(local.getCount());
+            if (!simulate)
+                setStackInSlot(slot, ItemStack.EMPTY.copy());
+            return newStack;
+        } else {
+            newStack.setCount(amount);
+            if (!simulate)
+                newLocal.setCount(local.getCount() - amount);
+            setStackInSlot(slot, newLocal);
+            return newStack;
+        }
+    }
+
+    @Override
+    public int getSlotLimit(int slot) {
+        return 64;
+    }
+
+    @Override
+    protected int getStackLimit(int slot, @Nonnull ItemStack stack) {
+        return Math.min(getSlotLimit(slot), stack.getMaxStackSize());
     }
 }
