16f7c23cfbdb6fb4edc662184a7fa1d47d9b4159
kristofersokk <kristofer.sokk@gmail.com>
2017-10-22 03:28:08 +0300

    version and serverproxy

--- src/main/java/com/timotheteus/raincontrol/util/ModUtil.java
+++ src/main/java/com/timotheteus/raincontrol/util/ModUtil.java
@@ -5,11 +5,12 @@ import java.util.logging.Logger;
 public final class ModUtil {
 	
 	public static final String MOD_ID = "raincontrol";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2";
     public static final String NAME = "Rain Control";
     public static final Logger LOGGER = Logger.getLogger(NAME);
     private static final String PROXY_BASE = "com.timotheteus.raincontrol.proxy.";
     public static final String ClIENT_PROXY = PROXY_BASE + "ClientProxy";
     public static final String COMMON_PROXY = PROXY_BASE + "CommonProxy";
+    public static final String SERVER_PROXY = PROXY_BASE + "ServerProxy";
 
 }
